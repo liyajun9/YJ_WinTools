d6dde14dbdb549826f9bba489f4919137d93ae5b
liyj <liyajun9@126.com>
2019-09-16 15:59:50 +0800

    make YLog easier to use

---
+++ Log/YLog.cpp
@@ -0,0 +1,160 @@
+#include "stdafx.h"
+#include "YLog.h"
+
+//Creat a global instance
+CYLogger theLogger(_T(""), false,  static_cast<int>(CYLogger::LogItem::DateTime) | static_cast<int>(CYLogger::LogItem::ThreadId), 3);
+
+CYLogger::CYLogger(tstring sLogFileDirectory, bool bAutoEndline ,int loggableItem, int nLogSavingDays):
+m_sDirectory(sLogFileDirectory),m_bAutoEndline(bAutoEndline),m_loggableItem(loggableItem), m_nLogSavingDays(nLogSavingDays)
+{
+	InitializeCriticalSection(&m_cs);
+	m_sCurrDate = CYTimeUtils::GetCurrDate(CYTimeUtils::Date_Format_3);
+
+	if(sLogFileDirectory.empty()){
+		m_stream.open(m_sCurrDate + _T(".log"), std::ofstream::out|std::ofstream::app|std::ios::binary);
+	}else{
+		_tmkdir(sLogFileDirectory.c_str());
+		m_stream.open(m_sDirectory + _T("\\") + m_sCurrDate + _T(".log"), std::ofstream::out|std::ofstream::app|std::ios::binary);
+	}
+}
+
+CYLogger::~CYLogger()
+{
+	m_stream.close();
+	DeleteCriticalSection(&m_cs);
+}
+
+void CYLogger::Log(const char* pszData, ...)
+{
+	std::string sData;
+	va_list argList;
+	va_start(argList, pszData);
+	size_t nSize = _vscprintf(pszData, argList);
+	if(sData.capacity() < (nSize + 1))
+		sData.resize(nSize + 1);
+
+	vsprintf((char*)sData.data(), pszData, argList);
+	va_end(argList);
+
+#if defined(UNICODE) || defined(_UNICODE)		
+	std::wstring sDataTmp = CYCharEncodings::MBToWChar(sData);
+	write(sDataTmp.c_str(), logLevel);
+#else
+	write(sData.c_str(), LogLevel::Info);
+#endif
+}
+
+void CYLogger::Log(LogLevel logLevel, const char* pszData, ...)
+{
+	std::string sData;
+	va_list argList;
+	va_start(argList, pszData);
+	size_t nSize = _vscprintf(pszData, argList);
+	if(sData.capacity() < (nSize + 1))
+		sData.resize(nSize + 1);
+
+	vsprintf((char*)sData.data(), pszData, argList);
+	va_end(argList);
+
+#if defined(UNICODE) || defined(_UNICODE)		
+	std::wstring sDataTmp = CYCharEncodings::MBToWChar(sData);
+	write(sDataTmp.c_str(), logLevel);
+#else
+	write(sData.c_str(), logLevel);
+#endif
+}
+
+void CYLogger::Log(const wchar_t* pwszData, ...)
+{
+	std::wstring sData;
+	va_list argList;
+	va_start(argList, pwszData);
+	size_t nSize = _vscwprintf(pwszData, argList);
+	if(sData.capacity() < (nSize + 2))
+		sData.resize(nSize + 2);
+
+	vswprintf((wchar_t*)sData.data(), pwszData, argList);
+	va_end(argList);
+
+#if defined(UNICODE) || defined(_UNICODE)
+	write(sData.c_str(), logLevel);
+#else
+	std::string sDataTmp = CYCharEncodings::WCharToMB(sData);
+	write(sDataTmp.c_str(), LogLevel::Info);
+#endif
+}
+
+void CYLogger::Log(LogLevel logLevel, const wchar_t* pwszData, ...)
+{
+	std::wstring sData;
+	va_list argList;
+	va_start(argList, pwszData);
+	size_t nSize = _vscwprintf(pwszData, argList);
+	if(sData.capacity() < (nSize + 2))
+		sData.resize(nSize + 2);
+
+	vswprintf((wchar_t*)sData.data(), pwszData, argList);
+	va_end(argList);
+
+#if defined(UNICODE) || defined(_UNICODE)
+	write(sData.c_str(), logLevel);
+#else
+	std::string sDataTmp = CYCharEncodings::WCharToMB(sData);
+	write(sDataTmp.c_str(), logLevel);
+#endif
+}
+
+void CYLogger::write(const TCHAR* pData, LogLevel logLevel)
+{
+	CYCriticalSectionLock(&m_cs, true);
+	static bool bIsNewOpen = true;
+	tstring sCurrDate = CYTimeUtils::GetCurrDate(CYTimeUtils::Date_Format_3);
+	if(0 != m_sCurrDate.compare(sCurrDate)){
+		m_stream.close();
+		m_stream.open(m_sDirectory + _T("\\") + m_sCurrDate + _T(".log"), std::ofstream::out|std::ofstream::app|std::ios::binary);
+		m_stream << _T("\r\n********************************New Log*********************************") << std::endl;
+
+		m_sCurrDate = sCurrDate;			
+	}else{
+		if(bIsNewOpen){
+			tstring sExpireFileName = m_sDirectory + _T("\\") + CYTimeUtils::GetAddedDate(0 - m_nLogSavingDays, CYTimeUtils::Date_Format_3) + _T(".log");
+			_tremove(sExpireFileName.c_str());
+
+			const std::locale cn_loc("chs");
+			m_stream.imbue(cn_loc);
+
+			m_stream << _T("\r\n********************************New Log*********************************") << std::endl;
+			bIsNewOpen = false;	
+		}
+	}
+
+	if(m_loggableItem & static_cast<int>(DateTime)){
+		m_stream<<CYTimeUtils::GetCurrDateTime(CYTimeUtils::Date_Format_3, true);
+	}
+	if(m_loggableItem & static_cast<int>(ThreadId)){
+		m_stream<<_T("[")<<GetCurrentThreadId()<<_T("]");
+	}
+	switch (logLevel)
+	{
+	case Info:
+		m_stream<<_T("[INF]");
+		break;
+	case Debug:
+		m_stream<<_T("[DBG]");
+		break;
+	case Warn:
+		m_stream<<_T("[WRN]");
+		break;
+	case Error:
+		m_stream<<_T("[ERR]");
+		break;
+	default:
+		m_stream<<_T("[INF]");
+		break;
+	}
+	if(m_bAutoEndline)
+		m_stream<<_T(" ")<<pData<<std::endl;
+	else
+		m_stream<<_T(" ")<<pData;
+	m_stream.flush();
+}
--- Log/YLog.h
+++ Log/YLog.h
@@ -20,6 +20,8 @@
 #include <wchar.h>
 #include <system_error>
 
+#define LOG_OUT theLogger.Log
+
 class CYLogger{
 public:
 	enum LogLevel
@@ -37,119 +39,21 @@ public:
 	};
 
 public:
-	CYLogger(tstring sLogFileDirectory, bool bAutoEndline = false,int loggableItem = static_cast<int>(DateTime) | static_cast<int>(ThreadId), int nLogSavingDays = 3):
-	m_sDirectory(sLogFileDirectory),m_bAutoEndline(bAutoEndline),m_loggableItem(loggableItem), m_nLogSavingDays(nLogSavingDays)
-	{
-		InitializeCriticalSection(&m_cs);
-		m_sCurrDate = CYTimeUtils::GetCurrDate(CYTimeUtils::Date_Format_3);
-		_tmkdir(sLogFileDirectory.c_str());
-		m_stream.open(m_sDirectory + _T("\\") + m_sCurrDate + _T(".log"), std::ofstream::out|std::ofstream::app|std::ios::binary);
-	}
+	CYLogger(tstring sLogFileDirectory,		// log directory. support only one sub-directory. empty indicates current path.
+						bool bAutoEndline = false,	// whether auto add end of line
+						int loggableItem = static_cast<int>(DateTime) | static_cast<int>(ThreadId), //prefix of each line includings
+						int nLogSavingDays = 3);			// saving log for days
+	~CYLogger();
 
-	~CYLogger()
-	{
-		m_stream.close();
-		DeleteCriticalSection(&m_cs);
-	}
+	void Log( const char* pszData, ...); //loglevel is info by default
+	void Log(LogLevel logLevel, const char* pszData, ...);	
 
-	void Log(LogLevel logLevel, const char* pszData, ...)
-	{
-		std::string sData;
-		va_list argList;
-		va_start(argList, pszData);
-		size_t nSize = _vscprintf(pszData, argList);
-		if(sData.capacity() < (nSize + 1))
-			sData.resize(nSize + 1);
-
-		vsprintf((char*)sData.data(), pszData, argList);
-		va_end(argList);
-
-#if defined(UNICODE) || defined(_UNICODE)		
-		std::wstring sDataTmp = CYCharEncodings::MBToWChar(sData);
-		write(sDataTmp.c_str(), logLevel);
-#else
-		write(sData.c_str(), logLevel);
-#endif
-	}
-
-	void Log(LogLevel logLevel, const wchar_t* pwszData, ...)
-	{
-		std::wstring sData;
-		va_list argList;
-		va_start(argList, pwszData);
-		size_t nSize = _vscwprintf(pwszData, argList);
-		if(sData.capacity() < (nSize + 2))
-			sData.resize(nSize + 2);
-
-		vswprintf((wchar_t*)sData.data(), pwszData, argList);
-		va_end(argList);
-		
-#if defined(UNICODE) || defined(_UNICODE)
-		write(sData.c_str(), logLevel);
-#else
-		std::string sDataTmp = CYCharEncodings::WCharToMB(sData);
-		write(sDataTmp.c_str(), logLevel);
-#endif
-	}
+	void Log(const wchar_t* pwszData, ...);
+	void Log(LogLevel logLevel, const wchar_t* pwszData, ...);
 
 private:
-
-	inline void write(const TCHAR* pData, LogLevel logLevel)
-	{
-		CYCriticalSectionLock(&m_cs, true);
-		static bool bIsNewOpen = true;
-		tstring sCurrDate = CYTimeUtils::GetCurrDate(CYTimeUtils::Date_Format_3);
-		if(0 != m_sCurrDate.compare(sCurrDate)){
-			m_stream.close();
-			m_stream.open(m_sDirectory + _T("\\") + m_sCurrDate + _T(".log"), std::ofstream::out|std::ofstream::app|std::ios::binary);
-			m_stream << _T("********************************New Log*********************************") << std::endl;
-
-			m_sCurrDate = sCurrDate;			
-		}else{
-			if(bIsNewOpen){
-				tstring sExpireFileName = m_sDirectory + _T("\\") + CYTimeUtils::GetAddedDate(0 - m_nLogSavingDays, CYTimeUtils::Date_Format_3) + _T(".log");
-				_tremove(sExpireFileName.c_str());
-
-				const std::locale cn_loc("chs");
-				m_stream.imbue(cn_loc);
-
-				m_stream << _T("********************************New Log*********************************") << std::endl;
-				bIsNewOpen = false;	
-			}
-		}
-
-		if(m_loggableItem & static_cast<int>(DateTime)){
-			m_stream<<CYTimeUtils::GetCurrDateTime(CYTimeUtils::Date_Format_3, true);
-		}
-		if(m_loggableItem & static_cast<int>(ThreadId)){
-			m_stream<<_T("[")<<GetCurrentThreadId()<<_T("]");
-		}
-		switch (logLevel)
-		{
-		case Info:
-			m_stream<<_T("[INF]");
-			break;
-		case Debug:
-			m_stream<<_T("[DBG]");
-			break;
-		case Warn:
-			m_stream<<_T("[WRN]");
-			break;
-		case Error:
-			m_stream<<_T("[ERR]");
-			break;
-		default:
-			m_stream<<_T("[INF]");
-			break;
-		}
-		if(m_bAutoEndline)
-			m_stream<<_T(" ")<<pData<<std::endl;
-		else
-			m_stream<<_T(" ")<<pData;
-		m_stream.flush();
-	}
-
-
+	CYLogger();
+	inline void write(const TCHAR* pData, LogLevel logLevel);
 
 private:
 	CRITICAL_SECTION m_cs;
@@ -162,3 +66,5 @@ private:
 
 	tofstream m_stream;
 };
+
+extern CYLogger theLogger;
