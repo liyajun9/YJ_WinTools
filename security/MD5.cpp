f6be1f473ef291810a193511bf0e9a9e8d689006
liyj <liyajun9@126.com>
2020-01-10 15:04:46 +0800

    MD5: add methods

--- security/MD5.cpp
+++ security/MD5.cpp
@@ -7,7 +7,14 @@
 #pragma comment(lib,"libssl.lib")
 
 using namespace std;
-std::string YMD5::Update(const std::string& sIn, size_t nLen, bool isToUpperCase)
+
+std::string YMD5::Update(const std::string& sIn, bool toUpperCase /*= true*/)
+{
+	size_t nLen = sIn.length();
+	return Update(sIn, nLen, toUpperCase);
+}
+
+std::string YMD5::Update(const std::string& sIn, size_t nLen, bool toUpperCase /*= true*/)
 {
 	unsigned char MD[MD5_DIGEST_LENGTH]; memset(&MD, 0, MD5_DIGEST_LENGTH);
 	MD5_CTX ctx = {0};
@@ -16,14 +23,21 @@ std::string YMD5::Update(const std::string& sIn, size_t nLen, bool isToUpperCase
 	MD5_Final(MD, &ctx);
 
 	char szTmp[MD5_BUFFER_SIZE]; memset(szTmp, 0, MD5_BUFFER_SIZE);
-	const char *fmt = isToUpperCase ? "%02X" : "%02x";
+	const char *fmt = toUpperCase ? "%02X" : "%02x";
 	for (unsigned int i = 0; i < MD5_BUFFER_SIZE/2; i++){
 		sprintf(szTmp + i * 2, fmt, MD[i]);
 	}
 	string sOut = szTmp;
 	return sOut;
 }
-void YMD5::Update(const std::string& sIn, size_t nLen, std::string& sOut,  bool isToUpperCase)
+
+void YMD5::Update(const std::string& sIn, std::string& sOut,  bool toUpperCase/*= true*/)
+{
+	size_t nLen = sIn.length();
+	Update(sIn, nLen, sOut, toUpperCase);
+}
+
+void YMD5::Update(const std::string& sIn, size_t nLen, std::string& sOut,  bool toUpperCase/*= true*/)
 {
 	unsigned char MD[MD5_DIGEST_LENGTH]; memset(&MD, 0, MD5_DIGEST_LENGTH);
 	MD5_CTX ctx = {0};
@@ -32,14 +46,14 @@ void YMD5::Update(const std::string& sIn, size_t nLen, std::string& sOut,  bool 
 	MD5_Final(MD, &ctx);
 
 	char szTmp[MD5_BUFFER_SIZE]; memset(szTmp, 0, MD5_BUFFER_SIZE);
-	const char *fmt = isToUpperCase ? "%02X" : "%02x";
+	const char *fmt = toUpperCase ? "%02X" : "%02x";
 	for (unsigned int  i = 0; i < MD5_BUFFER_SIZE/2; i++){
 		sprintf(szTmp + i * 2, fmt, MD[i]);
 	}
 	sOut = szTmp;
 }
 
-void YMD5::Update(const void* pIn, unsigned int nCbLen, char* pszOut, unsigned int nCbOutLen, bool isToUpperCase)
+void YMD5::Update(const void* pIn, unsigned int nCbLen, char* pszOut, unsigned int nCbOutLen, bool toUpperCase/*= true*/)
 {
 	if(nCbOutLen < MD5_BUFFER_SIZE)	return;
 
@@ -49,7 +63,7 @@ void YMD5::Update(const void* pIn, unsigned int nCbLen, char* pszOut, unsigned i
 	MD5_Update(&ctx, pIn, nCbLen);
 	MD5_Final(MD, &ctx);
 
-	const char *fmt = isToUpperCase ? "%02X" : "%02x";
+	const char *fmt = toUpperCase ? "%02X" : "%02x";
 	for (unsigned int  i = 0; i < MD5_BUFFER_SIZE/2; i++){
 		sprintf(pszOut + i * 2, fmt, MD[i]);
 	}
--- security/MD5.h
+++ security/MD5.h
@@ -5,9 +5,16 @@
 
 class YMD5{
 public:
-	static std::string Update(const std::string& sIn, size_t nLen, bool isToUpperCase);
-	static void Update(const std::string& sIn, size_t nLen, std::string& sOut, bool isToUpperCase);
-	static void Update(const void* pIn, unsigned int nCbLen, char* pszOut, unsigned int nCbOutLen, bool isToUpperCase);
+	static std::string Update(const std::string& sIn, bool toUpperCase = true);
+	static std::string Update(const std::string& sIn, size_t nLen, bool toUpperCase = true);
+
+	static void Update(const std::string& sIn, std::string& sOut, bool toUpperCase = true);
+	static void Update(const std::string& sIn, size_t nLen, std::string& sOut, bool toUpperCase = true);
+
+	/*
+	Note: Nothing happends if nCbOutLen < MD5_BUFFER_SIZE
+	*/
+	static void Update(const void* pIn, unsigned int nCbLen, char* pszOut, unsigned int nCbOutLen, bool toUpperCase = true);
 
 private:
 	YMD5(){}
